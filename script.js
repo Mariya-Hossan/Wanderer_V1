7205f7a5bf49449da7df9b55521e757369ac28eb
Mariya-Hossan <113036070+Mariya-Hossan@users.noreply.github.com>
2024-09-18 21:53:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+ function show(){
+    alert("This page is now in developing stage.Thank you")
+ }
+
+ function getalert(){
+   alert("The option called see more will be activated later. Thank you")
+ }
